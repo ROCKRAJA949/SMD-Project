b02c262dc786da55e99bec365e09f1c555027e8a
hussainarslan <hussain.chawla@gmail.com>
2023-12-13 16:12:47 +0500

    real time database for join and start game to maintain session and updates for move

--- app/src/main/java/com/example/tic_tac_toechallenge/presentation/authentication/GameResponseModel.kt
+++ app/src/main/java/com/example/tic_tac_toechallenge/presentation/authentication/GameResponseModel.kt
@@ -1,11 +1,14 @@
 package com.example.tic_tac_toechallenge.presentation.authentication
 
 data class GameResponseModel (
-    val player1Id: String,
-    val player2Id: String?,
-    val status: String,
-    val turn: String,
-    val winnerId: String,
-    val boardState: List<String>
+    val player1Id: String = "",
+    val player2Id: String? = null,
+    val status: String = "",
+    val turn: String = "",
+    val winnerId: String = "",
+    val boardState: List<String> = emptyList()
 )
-
+{
+    // No-argument constructor
+    constructor() : this("", null, "", "", "", emptyList())
+}
---
+++ app/src/main/java/com/example/tic_tac_toechallenge/presentation/authentication/JoinGameRequestModel.kt
@@ -0,0 +1,6 @@
+package com.example.tic_tac_toechallenge.presentation.authentication
+
+data class JoinGameRequestModel (
+    val id: String,
+    val playerId: String
+)
